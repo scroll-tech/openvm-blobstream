e65c4b8ac46a6a48a1b8aed3af59127e6dadd247
lightsing <light.tsing@gmail.com>
2025-10-27 14:04:37 +0800

    openvm example

--- openvm/program/src/main.rs
+++ openvm/program/src/main.rs
@@ -1,8 +1,11 @@
-use openvm_blobstream::guest::GuestInput;
+extern crate openvm_keccak256_guest;
+
+use openvm_blobstream::GuestInput;
 
 openvm::entry!(main);
 openvm::init!();
 
 fn main() {
+    openvm_blobstream::install_revm_crypto(openvm_blobstream::openvm::Crypto);
     openvm_blobstream::guest::validate(openvm::io::read::<GuestInput>()).unwrap();
 }
